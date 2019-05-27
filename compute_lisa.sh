8534436a804876aab04f0ae401d3dcc255cd8d90
Raoul Schram <r.d.schram@uu.nl>
2019-05-27 11:56:23 +0200

    Fix batch script

--- compute_lisa.sh
+++ compute_lisa.sh
@@ -20,7 +20,7 @@ rm -f $COMMAND_FILE
 
 let "NJOB_MAX=N_JOBS-1"
 for JOB in `seq 0 $NJOB_MAX`; do
-    echo "\${python} ./streetgreen.py amsterdam_almere --njobs $N_JOBS --jobid $JOB --model deeplab-xception_71 -l 2" >> $COMMAND_FILE
+    echo "\${python} ./streetgreen.py --bbox amsterdam_almere --njobs $N_JOBS --jobid $JOB --model deeplab-xception_71 -l 2" >> $COMMAND_FILE
 done
 
 batchgen -f $COMMAND_FILE $CONFIG_FILE -pre $PRE_FILE
