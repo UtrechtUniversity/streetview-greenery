97086fde6cdee713daa2c515261d4e1128305cf4
Raoul Schram <r.d.schram@uu.nl>
2019-07-31 13:41:48 +0200

    Update kriging stage on Lisa

--- krige_lisa.sh
+++ krige_lisa.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
+LVL=6
+
+CATEGORIES=(
+    "road" "sidewalk" "building" 
+    "wall" "fence" "pole" 
+    "traffic light" "traffic sign" 
+    "vegetation" "terrain" "sky" 
+    "person" "rider" "car" 
+    "truck" "bus" "train" 
+    "motorcycle" "bicycle"
+)
+
+
 COMMAND_FILE="temp_commands.txt"
 PRE_FILE="temp_pre.txt"
 CONFIG_FILE="krige_lisa.ini"
+rm -f $COMMAND_FILE
 
 cat > $PRE_FILE << EOF_CAT
 cd `pwd`
 source hpc/module_load_cpu.sh
 EOF_CAT
 
-echo "\${python} ./streetgreen.py --bbox amsterdam_almere -l 4" > $COMMAND_FILE
+for CAT in ${CATEGORIES[*]}; do
+    echo "\${python} ./streetgreen.py --bbox amsterdam_almere -l $LVL -g $CAT --model deeplab-xception_71" >> $COMMAND_FILE
+done
 
 batchgen -f $COMMAND_FILE $CONFIG_FILE -pre $PRE_FILE
 
