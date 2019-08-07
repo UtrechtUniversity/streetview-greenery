7866baa7f9cc76bdfb62102e1343910b918f0791
Raoul Schram <r.d.schram@uu.nl>
2019-08-07 17:23:42 +0200

    Update kriging script to do it in parallel

--- krige_lisa.sh
+++ krige_lisa.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 LVL=6
+N_JOBS=16
 
 declare -a CATEGORIES=(
     "road" "sidewalk" "building" 
@@ -22,9 +23,14 @@ cd `pwd`
 source hpc/module_load_cpu.sh
 EOF_CAT
 
-for CAT in "${CATEGORIES[@]}"; do
-    echo "\${python} ./streetgreen.py --bbox amsterdam_almere -l $LVL -g '$CAT' --model deeplab-xception_71" >> $COMMAND_FILE
+let "NJOB_MAX=N_JOBS-1"
+CAT="vegetation"
+for JOB in `seq 0 $NJOB_MAX`; do
+    echo "\${python} ./streetgreen.py --bbox amsterdam_almere -l $LVL -g '$CAT' --model deeplab-xception_71 --njobs ${N_JOBS} --jobid $JOB" >> $COMMAND_FILE
 done
+# for CAT in "${CATEGORIES[@]}"; do
+#     echo "\${python} ./streetgreen.py --bbox amsterdam_almere -l $LVL -g '$CAT' --model deeplab-xception_71" >> $COMMAND_FILE
+# done
 
 batchgen -f $COMMAND_FILE $CONFIG_FILE -pre $PRE_FILE
 
