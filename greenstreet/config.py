22a7a3329a85e0d7169d0547f8219ba5e2539c74
Raoul Schram <r.d.schram@uu.nl>
2020-02-07 11:28:55 +0100

    Add config file

---
+++ greenstreet/config.py
@@ -0,0 +1,9 @@
+PICTURE_NAMES = {
+    "adam-panorama": ["adam-panorama.jpg"],
+    "adam-cubic": ["adam-front.jpg", "adam-back.jpg", "adam-left.jpg",
+                   "adam-right.jpg"],
+}
+
+INV_PICTURE_NAMES = {}
+for pic_type, files in PICTURE_NAMES.items():
+    INV_PICTURE_NAMES.update({file: pic_type for file in files})
